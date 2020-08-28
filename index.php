0569b2c3840f06209393da362403523895f25a3b
loichobe <66281116+loichobe@users.noreply.github.com>
2020-08-28 15:57:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if(isset($_POST['email']) AND $_POST['email'] != '') {
         $country = $_POST['country'];
         $email = $_POST['email'];
         $comment = $_POST['comment'];
-
+    }
 
 }
 ?>
