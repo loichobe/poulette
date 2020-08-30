934282a13962295974c97a9a3c99cd7815c37dd0
loichobe <66281116+loichobe@users.noreply.github.com>
2020-08-30 22:23:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,31 +19,61 @@ if(isset($_POST['email']) AND $_POST['email'] != '') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/styles.css">
     <title>Hackers Poulette</title>
 </head>
-<body>
-        <form>
-        <label for="firstname">First name :</label>
-        <input type="text" name="firstname" required placeholder="Your first name" alt="First name">
-        <label for="lastname"> Last name :</label>
-        <input type="text" name="lastname" required placeholder="Your last name" alt="Last name"><br><br>
-        <label for="gender">Gender</label><br>
-        <input type="radio" name="gender" value="male" alt="male"> Male
-        <input type="radio" name="gender" value="female" alt="female"> Female<br><br>
-        <label for="email">Email adress :</label>
-        <input type="email" required name="email" placeholder="exemple@email.com" alt="Email">
-        <label for="country">Country</label>
-        <input type="text" required name="country" placeholder="Country" alt="Country">
-        <label for="subject">Subject :</label>
-        <select name="subject" required alt="What is the subject?" >
-            <option value="Other" alt="other">Other</option>
-            <option value="Order" alt="order">Order</option>
-            <option value="Claim" alt="claim">Claim</option>
-        </select><br>
-        <label for="comment">Message :</label>
-        <textarea name="comment" id="comment"></textarea></textarea>
-        <input type="submit" value="Confirm" name="confirm" alt="Confirm">
+<body color="info">
+    <div class="container">
+    <form  action="#" method="POST" class="form">
+    <img src="./assets/img/hackers.png" id="logo">     
+            <div class="form-group">
+                <label for="name">Last name :</label>
+                <input type="text" name="lastname" class="form-control"placeholder="Type your last name" required>
+             </div>
+                
+            <div class="form-group">
+                <label for="first-name">First name :</label>
+                <input type="text" name="firstname" class="form-control" placeholder="Type your first name" required>
+            </div>
+                            
+            <div class="form-group">
+                <label for="email">Email :</label>
+                <input type="email" id="email" name="email" class="form-control" placeholder="example@email.com" required >
+            </div>
 
+            <div class="form-group">
+                <label for="country">Country :</label>
+                <input type="text" id="country" name="country" class="form-control" placeholder="Type your country" required>
+            </div>
+
+            <div class="form-group">
+                <label for="gender">Gender</label><br>
+                <input type="radio" name="gender" value="male" alt="male"> Male
+                <input type="radio" name="gender" value="female" alt="female"> Female
+            </div>
+                                                    
+            <div class="form-group">                          
+                <label for="subject">Subject :</label>
+                <select name="subject" required alt="What is the subject?" >
+                <option value="Other" alt="other">Other</option>
+                <option value="Order" alt="order">Order</option>
+                <option value="Claim" alt="claim">Claim</option>
+            </div>
+                
+                                                                                
+            <div class="form-group">
+                <label for="message">Message</label>
+                <textarea type="text" id="comment" name="comment" class="form-control" placeholder="Type your message here" required></textarea>
+            </div> 
+            <div class="form-group center">
+                <input type="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModal" value="Send">
+            </div>
+                                
     </form>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
 </html>
